5daf02388ddb1389103a50b830cf56de337fca94
Diana <diana.lemen@gmail.com>
2020-03-16 22:27:04 +0200

    Updates

---
+++ precache-manifest.4b62f87e438f73c7b0d295579da3f4d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ac0135d0fddc2ae2446d3439420f527",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ad3655659a4b27c6f7d9",
+    "url": "/test/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "7a1d55395b6480a542d7",
+    "url": "/test/static/js/2.03a7b3b8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/test/static/js/2.03a7b3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3655659a4b27c6f7d9",
+    "url": "/test/static/js/main.eeeb8a58.chunk.js"
+  },
+  {
+    "revision": "52eb28ac3a82c29a4f18",
+    "url": "/test/static/js/runtime-main.eef85520.js"
+  }
+]);
